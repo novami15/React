b8d558d848509138ab75a4060dd67cf32c664de2
novami15 <ngovanminh15062002@gmail.com>
2023-02-21 15:42:02 +0700

    Components Part 2

--- confusions/src/App.js
+++ confusions/src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
   }
 }
+  <Menu dishes={this.state.dishes} />
 
-<Menu />
 export default App;
